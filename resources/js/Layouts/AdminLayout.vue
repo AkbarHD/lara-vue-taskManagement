309ae16e850dfdad654b08a0079a8152a5216ff2
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2025-01-23 13:05:01 +0700

    flowbite dan sign out

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <Navbar />
+        <!-- slot itu berfungsi untuk menampung konten -->
+        <slot />
+    </div>
+</template>
+<script setup>
+import Navbar from '@/Components/layout/Navbar.vue';
+</script>
---
+++ resources/js/Pages/Admin/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <AdminLayout>
+        <div>
+            <h1 class="text-2xl">Page Home</h1>
+        </div>
+    </AdminLayout>
+</template>
+
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+</script>
